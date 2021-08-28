7ab019b502f8638ecc21926794c244633d5f5207
Elcee <elcee.github@gmail.com>
2021-08-28 22:13:03 +0100

    Add a new file to load the libraries via pragma

---
+++ src/Game/libs.c
@@ -0,0 +1,8 @@
+
+
+#pragma comment( lib, "C:/Projects/HomeworldSDL/SDL/VisualC/Win32/Debug/SDL2.lib")
+#pragma comment( lib, "C:/Projects/HomeworldSDL/SDL/VisualC/Win32/Debug/SDL2main.lib")
+#pragma comment( lib, "opengl32.lib")
+
+
+
