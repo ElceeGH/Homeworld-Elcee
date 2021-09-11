1c0b1e426ea15cd69548a8e55c2a56e8e9644aa0
Elcee <elcee.github@gmail.com>
2021-09-11 07:24:50 +0100

    Remove unnecessary LibAV libraries.

--- src/Game/libs.c
+++ src/Game/libs.c
@@ -1,14 +1,11 @@
 
 
+
 #pragma comment( lib, "C:/Projects/HomeworldSDL/SDL/VisualC/Win32/Debug/SDL2.lib")
 #pragma comment( lib, "C:/Projects/HomeworldSDL/SDL/VisualC/Win32/Debug/SDL2main.lib")
 #pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avcodec.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avdevice.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avfilter.lib")
 #pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avformat.lib")
 #pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avutil.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/postproc.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/swresample.lib")
 #pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/swscale.lib")
 #pragma comment( lib, "opengl32.lib")
 
