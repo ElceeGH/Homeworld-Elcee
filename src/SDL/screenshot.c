59c85c06f456c04de61494f33f14585c339be4e9
Elcee <elcee.github@gmail.com>
2021-09-11 19:20:04 +0100

    Screenshots: - Clean up the code generally. - Remove dependency on VirtualAlloc/mmap.  Malloc is totally fine. - Don't try to prevent overwriting files or sleeping to limit the rate. It doesn't matter. - Fix the filenames having stupid stuff like "GMT +0000" after the name because of  %Z.

--- src/SDL/screenshot.c
+++ src/SDL/screenshot.c
@@ -7,156 +7,124 @@
 // =============================================================================
 
 #include "screenshot.h"
-
-#ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
-    #include <windows.h>
-#else
-    #include <sys/mman.h>
-#endif
-
 #include "Debug.h"
 #include "glinc.h"
 #include "interfce.h"
 #include "main.h"
-
-#ifdef _WIN32
-	#define sleep(x) _sleep((x) * 1000)
-#else
-    #include <unistd.h>
-#endif
-
-static void _ssAppendScreenshotFilename(char* savePath);
-static void _ssSaveScreenshot(ubyte* buf);
+#include <stdbool.h>
 
 
-// =============================================================================
 
+static void ssGenScreenshotFilename( char* filepath, const size_t filepathSize );
+static bool ssVerticallyFlipImage( ubyte* buffer, const size_t width, const size_t height, const size_t bpp );
+static void ssSaveScreenshot( ubyte* buffer, const size_t width, const size_t height );
 
-static void _ssAppendScreenshotFilename(char* savePath)
-{
-    FILE        *imageFile;
-    char         imagePath[PATH_MAX + 1],
-                 imageName[256];
-                 
-    time_t       now;
-    struct tm    timeStruct;
-    
-    unsigned int attempts          =  0,
-                 sleep_time_secs   =  1,
-                 max_attempts_secs = 10;
-
-    while (1) {
-        time(&now);
-        timeStruct = *localtime(&now);
-        
-        strftime(imageName, sizeof(imageName), "shot_%Y%m%d_%H%M%S_%Z.jpg", &timeStruct);
-
-        strcpy(imagePath, savePath);
-        strcat(imagePath, imageName);
-        
-        // unable to open filename so presumably good name to use
-        if ((imageFile = fopen(imagePath, "r")) == NULL) {
-            break;
-        }
-        
-        fclose(imageFile);
-        attempts++;
-        sleep(sleep_time_secs);    // rapid screenshots possibly: wait and try again
-
-        // don't try for more than the allowed time
-        if (attempts > max_attempts_secs / sleep_time_secs) {
-            strcpy(imageName, "most_recent_screenshot.jpg");
-            break;
-        }
-    }
-
-    strcat(savePath, imageName);
-}
-
-
-static void _ssSaveScreenshot(ubyte* buf)
-{
-    char *fname;
-    FILE* out;
-    unsigned char *pTempLine;
-    long Top, Bot, i, Size;
-
-    JPEGDATA jp;
-
-    fname = filePathPrepend("ScreenShots/", FF_UserSettingsPath);
-    if (!fileMakeDirectory(fname))
-        return;
-
-    _ssAppendScreenshotFilename(fname);
-
-#if SS_VERBOSE_LEVEL >= 1
-    dbgMessagef("Saving %dx%d screenshot to '%s'.", MAIN_WindowWidth, MAIN_WindowHeight, fname);
-#endif
-
-    out = fopen(fname, "wb");
-    if (out == NULL)
-    {
-        return;
-    }
-
-    Size = MAIN_WindowWidth*3;
-    pTempLine = (unsigned char *)malloc(Size);
-
-    for (i = 0; i < (MAIN_WindowHeight / 2); i++)
-    {
-        Top = i;
-        Bot = (MAIN_WindowHeight - 1) - i;
-
-        memcpy(pTempLine, buf + (Size * Top), Size);
-        memcpy(buf + (Size * Top), buf + (Size * Bot), Size);
-        memcpy(buf + (Size * Bot), pTempLine, Size);
-    }
-
-    free(pTempLine);
-
-    // Fill out the JPG lib info structure
-    memset(&jp, 0, sizeof(jp));
-
-    jp.ptr = buf;
-    jp.width = MAIN_WindowWidth;
-    jp.height = MAIN_WindowHeight;
-    jp.output_file = out;
-    jp.aritcoding = 0;
-    jp.quality = 97;
-
-    JpegWrite(&jp);
-
-    fclose(out);
-}
 
 
 void ssTakeScreenshot(void)
 {
-    ubyte* screenshot_buffer =
-#ifdef _WIN32
-        (void *)VirtualAlloc(NULL, 3 * MAIN_WindowWidth * MAIN_WindowHeight,  // 3 = RGB
-            MEM_COMMIT, PAGE_READWRITE);
-#else
-        mmap(NULL, 3 * MAIN_WindowWidth * MAIN_WindowHeight,
-            PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
-#endif
+    // Determine params
+    const size_t width  = MAIN_WindowWidth;
+    const size_t height = MAIN_WindowHeight;
+    const size_t bpp    = 3; // RGB8
+    const size_t bytes  = width * height * bpp;
 
-    if (screenshot_buffer != NULL)
-    {
-        int result = 0;
-        
-        glReadPixels(0, 0, MAIN_WindowWidth, MAIN_WindowHeight,
-            GL_RGB, GL_UNSIGNED_BYTE, screenshot_buffer);
-            
-        _ssSaveScreenshot(screenshot_buffer);
+    // Allocate the buffer
+    ubyte* const buffer = malloc( bytes );
+    if (buffer == NULL)
+        return;
 
-#ifdef _WIN32
-        result = VirtualFree(screenshot_buffer, 0, MEM_RELEASE);
-        dbgAssertOrIgnore(result);
-#else
-        result = munmap(screenshot_buffer, 3*MAIN_WindowWidth*MAIN_WindowHeight);
-        dbgAssertOrIgnore(result != -1);
-#endif
+    // Read data from GL
+    glPixelStorei( GL_PACK_ALIGNMENT, 1 );
+    glReadPixels( 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, buffer );
+
+    // Vertically flip image
+    if ( ! ssVerticallyFlipImage( buffer, width, height, bpp))
+        return;
+
+    // Save it and free memory
+    ssSaveScreenshot( buffer, width, height );
+    free( buffer );
+}
+
+
+
+/// OpenGL stores images with origin in the lower left.
+/// Pretty much everything else places it at the top left.
+/// Returns whether it succeeded.
+static bool ssVerticallyFlipImage( ubyte* data, const size_t width, const size_t height, const size_t bpp )
+{
+    const size_t rows       = height;
+    const size_t rowBytes   = width * bpp;
+    const size_t imageBytes = rowBytes * rows;
+
+    ubyte* const rowStart = data;
+    ubyte* const rowEnd   = rowStart + imageBytes - rowBytes;
+    ubyte* const rowT     = malloc( rowBytes );
+
+    if (rowT == NULL)
+        return FALSE;
+
+    for (size_t rowOffset=0; rowOffset<rows/2; rowOffset++) {
+        ubyte* const rowA = rowStart + (rowOffset * rowBytes);
+        ubyte* const rowB = rowEnd   - (rowOffset * rowBytes);
+
+        memcpy( rowT, rowA, rowBytes ); // A -> T
+        memcpy( rowA, rowB, rowBytes ); // B -> A
+        memcpy( rowB, rowT, rowBytes ); // T -> B
     }
+
+    free( rowT );
+    return TRUE;
+}
+
+
+
+static void ssGenScreenshotFilename( char* filepath, const size_t filepathSize ) 
+{
+    time_t now = 0;
+    time(&now);
+
+    char filename[ PATH_MAX ] = { '\0' };
+    strftime( filename, sizeof(filename), "shot_%Y%m%d_%H%M%S.jpg", localtime(&now) );
+    snprintf( filepath, filepathSize, "%s%s/%s", fileUserSettingsPath, "ScreenShots/", filename );
+}
+
+
+
+static void ssSaveScreenshot( ubyte* const buffer, const size_t width, const size_t height )
+{
+    // Generate the name
+    char filepath[ MAX_PATH ] = { '\0' };
+    ssGenScreenshotFilename( filepath, sizeof(filepath) );
+
+    // Create directory structure if required
+    if ( ! fileMakeDestinationDirectory(filepath)) {
+        dbgMessagef( "Couldn't create screenshot directory! Path: %s", filepath );
+        return;
+    }
+    
+    // Open the file
+    FILE* out = fopen( filepath, "wb" );
+    if (out == NULL) {    
+        dbgMessagef( "Couldn't open screenshot file! Path: %s", filepath );
+        return;
+    }
+
+    // Show what's happening
+    dbgMessagef( "Saving %dx%d screenshot to '%s'", width, height, filepath );
+
+    // Fill out the JPG lib info structure
+    JPEGDATA jp = {
+        .ptr         = buffer,
+        .width       = width,
+        .height      = height,
+        .output_file = out,
+        .aritcoding  = 0,
+        .quality     = 97,
+    };
+
+    // Write and close.
+    JpegWrite( &jp );
+    fclose( out );
 }
