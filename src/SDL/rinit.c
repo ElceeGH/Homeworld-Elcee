d671e8cc2181c310d54ad6cf924aaa619e7a398d
Elcee <elcee.github@gmail.com>
2021-09-14 20:15:39 +0100

    rinit: Simplify the code, lots of this is never used. Also assume standard HD resolutions 720p and 1080p are supported if mode enumeration fails.

--- src/SDL/rinit.c
+++ src/SDL/rinit.c
@@ -26,21 +26,6 @@ extern unsigned int mainSoftwareDirectDraw;
 extern unsigned int mainOutputCRC;
 extern unsigned long strCurLanguage;
 
-#define RIN_MODESINDEVSTATLIST 10
-unsigned int rinDevstatModeTable[RIN_MODESINDEVSTATLIST][3] =
-{
-    { 16,  640, DEVSTAT_NO_64048016 },
-    { 16,  800, DEVSTAT_NO_80060016 },
-    { 16, 1024, DEVSTAT_NO_102476816 },
-    { 16, 1280, DEVSTAT_NO_1280102416 },
-    { 16, 1600, DEVSTAT_NO_1600120016 },
-    { 32,  640, DEVSTAT_NO_64048032 },
-    { 32,  800, DEVSTAT_NO_80060032 },
-    { 32, 1024, DEVSTAT_NO_102476832 },
-    { 32, 1280, DEVSTAT_NO_1280102432 },
-    { 32, 1600, DEVSTAT_NO_1600120032 },
-};
-
 extern unsigned int* devTable;
 extern int devTableLength;
 
@@ -50,16 +35,6 @@ static int nDevices;
 unsigned int gDevcaps  = 0xFFFFFFFF;
 unsigned int gDevcaps2 = 0x00000000;
 
-#if 0	/* Direct3D stuff...not used. */
-char* gDescription = NULL;
-char* gDriverName = NULL;
-
-typedef HRESULT(WINAPI * DIRECTDRAWCREATE)(GUID*, LPDIRECTDRAW*, IUnknown*);
-typedef HRESULT(WINAPI * DIRECTINPUTCREATE)(HINSTANCE, DWORD, LPDIRECTINPUT*, IUnknown*);
-#endif
-
-extern unsigned int sstHardwareExists(int*);
-extern unsigned int glCapNT(void);
 
 static void* rinMemAlloc(int size)
 {
@@ -127,10 +102,7 @@ unsigned int rinDeviceCRC(void)
 ----------------------------------------------------------------------------*/
 void rinAddMode(rdevice* dev, int width, int height, int depth)
 {
-    rmode* mode;
-    rmode* cmode;
-
-    mode = (rmode*)rinMemAlloc(sizeof(rmode));
+    rmode* mode = rinMemAlloc(sizeof(rmode));
     mode->width  = width;
     mode->height = height;
     mode->depth  = depth;
@@ -144,7 +116,7 @@ void rinAddMode(rdevice* dev, int width, int height, int depth)
     else
     {
         //add to tail
-        cmode = dev->modes;
+        rmode* cmode = dev->modes;
         while (cmode->next != NULL)
         {
             cmode = cmode->next;
@@ -164,48 +136,9 @@ void rinAddMode(rdevice* dev, int width, int height, int depth)
 ----------------------------------------------------------------------------*/
 static bool rinModeAccepted(rdevice* dev, int width, int height, int depth)
 {
-    int index;
-
-    //accept 16, 24, or 32 bit mode only
-    if (depth != 16 && depth != 24 && depth != 32)
-    {
-        return FALSE;
-    }
-
-    //possibly eliminate 32bit modes, if devstats tells us to
-    if (depth == 32)
-    {
-        if (dev->type == RIN_TYPE_DIRECT3D)
-        {
-            if (dev->devcaps & DEVSTAT_NO_32BIT)
-            {
-                return FALSE;
-            }
-        }
-        else if (dev->type == RIN_TYPE_OPENGL)
-        {
-            if ((dev->devcaps & DEVSTAT_NO_32BIT_GL) ||
-                (dev->devcaps & DEVSTAT_NO_32BIT))
-            {
-                return FALSE;
-            }
-        }
-    }
-	
-    //check for specifically disabled display modes
-    for (index = 0; index < RIN_MODESINDEVSTATLIST; index++)
-    {
-        if (rinDevstatModeTable[index][0] == depth &&
-            rinDevstatModeTable[index][1] == width)
-        {
-            if (dev->devcaps & rinDevstatModeTable[index][2])
-            {
-                return FALSE;
-            }
-        }
-    }
-
-    return TRUE;
+    return depth == 16
+        || depth == 24
+        || depth == 32;
 }
 
 /*-----------------------------------------------------------------------------
@@ -260,66 +193,6 @@ void rinSortModes(rdevice* dev)
 
     //attach new modes
     dev->modes = dummy.modes;
-}
-
-/*-----------------------------------------------------------------------------
-    Name        : rinCopyModes
-    Description : copy one driver's display modes to another
-    Inputs      : devOut - output device
-                  devIn - input device
-    Outputs     :
-    Return      :
-----------------------------------------------------------------------------*/
-void rinCopyModes(rdevice* devOut, rdevice* devIn)
-{
-    rmode* cmode;
-
-    cmode = devIn->modes;
-    if (cmode == NULL)
-    {
-        devOut->modes = NULL;
-    }
-    else
-    {
-        do
-        {
-            rinAddMode(devOut, cmode->width, cmode->height, cmode->depth);
-            cmode = cmode->next;
-        } while (cmode != NULL);
-    }
-}
-
-/*-----------------------------------------------------------------------------
-    Name        : rinCopyModesSelectively
-    Description : copy certain display modes from one device to another
-    Inputs      : devOut - output device
-                  devIn - input device
-                  depth - bitdepth of acceptable modes
-                  maxWidth - maximum acceptable width
-    Outputs     :
-    Return      :
-----------------------------------------------------------------------------*/
-void rinCopyModesSelectively(rdevice* devOut, rdevice* devIn, int depth, int maxWidth)
-{
-    rmode* cmode;
-
-    cmode = devIn->modes;
-    if (cmode == NULL)
-    {
-        devOut->modes = NULL;
-    }
-    else
-    {
-        do
-        {
-            if ((cmode->depth == depth) &&
-                (cmode->width <= maxWidth))
-            {
-                rinAddMode(devOut, cmode->width, cmode->height, cmode->depth);
-            }
-            cmode = cmode->next;
-        } while (cmode != NULL);
-    }
 }
 
 /*-----------------------------------------------------------------------------
@@ -398,43 +271,6 @@ int rinFreeDevices(void)
 }
 
 /*-----------------------------------------------------------------------------
-    Name        : rinMaxWidth
-    Description : return max width from all modes of all devices
-    Inputs      :
-    Outputs     :
-    Return      : max available width, or 9999 if no devices available
-----------------------------------------------------------------------------*/
-int rinMaxWidth(void)
-{
-    rdevice* cdev;
-    rmode* cmode;
-    int maxWidth;
-
-    if (nDevices == 0)
-    {
-        return 9999;
-    }
-
-    cdev = rDeviceList;
-    maxWidth = 0;
-    while (cdev != NULL)
-    {
-        cmode = cdev->modes;
-        while (cmode != NULL)
-        {
-            if (cmode->width > maxWidth)
-            {
-                maxWidth = cmode->width;
-            }
-            cmode = cmode->next;
-        }
-        cdev = cdev->next;
-    }
-
-    return (maxWidth == 0) ? 9999 : maxWidth;
-}
-
-/*-----------------------------------------------------------------------------
     Name        : rinEnumeratePrimary
     Description : enumerate available display modes on the primary display
     Inputs      : dev - the device whose modes are to be filled
@@ -445,13 +281,10 @@ bool rinEnumeratePrimary(rdevice* dev)
 {
 	SDL_DisplayMode mode;
     Uint32 flags;
-	int max_width;
 	const int display_index = 0;
 
 	if (!dev)
 		return FALSE;
-
-	max_width = rinMaxWidth();
 
 	/* Make sure SDL video is initialized. */
 	flags = SDL_WasInit(SDL_INIT_EVERYTHING);
@@ -489,88 +322,31 @@ bool rinEnumeratePrimary(rdevice* dev)
 ----------------------------------------------------------------------------*/
 int rinEnumerateDevices(void)
 {
-    rdevice* dev;
-    rdevice* gldev;
-    rdevice  primaryDev;
-    bool primaryVal;
+    rdevice* dev = rinMemAlloc(sizeof(rdevice));
 
-    rDeviceList = NULL;
-    nDevices = 0;
-    gDevcaps  = 0xFFFFFFFF;
-    gDevcaps2 = 0x00000000;
-
-    //add OpenGL device
-    dev = (rdevice*)rinMemAlloc(sizeof(rdevice));
-    if (nDevices == 1)
-    {
-        dev->devcaps  = rDeviceList->devcaps;
-        dev->devcaps2 = rDeviceList->devcaps2;
-    }
-    else if (nDevices == 2)
-    {
-        dev->devcaps  = rDeviceList->next->devcaps;
-        dev->devcaps2 = rDeviceList->next->devcaps2;
-    }
-    else if (gDevcaps != 0xFFFFFFFF)
-    {
-        dev->devcaps  = gDevcaps;
-        dev->devcaps2 = gDevcaps2;
-    }
-    else
-    {
-        dev->devcaps = gDevcaps = 0;
-        dev->devcaps2 = gDevcaps2 = 0;
-    }
-    if (dev->devcaps & DEVSTAT_NO_DDRAWSW)
-    {
-        mainSoftwareDirectDraw = 0;
-    }
     dev->type = RIN_TYPE_OPENGL;
-    dev->data[0] = '\0';
-    if (strCurLanguage == 1)
-    {
-        strncpy(dev->name, "OpenGL Standard", 63);
+
+    switch (strCurLanguage) {
+        case 1:  snprintf( dev->name, sizeof(dev->name), "%s", "OpenGL Standard"       ); break;
+        case 2:  snprintf( dev->name, sizeof(dev->name), "%s", "Standard-OpenGL"       ); break;
+        case 3:  snprintf( dev->name, sizeof(dev->name), "%s", "OpenGL predeterminado" ); break;
+        case 4:  snprintf( dev->name, sizeof(dev->name), "%s", "OpenGL di Default"     ); break;
+        default: snprintf( dev->name, sizeof(dev->name), "%s", "Default OpenGL"        ); break;
     }
-    else if (strCurLanguage == 2)
-    {
-        strncpy(dev->name, "Standard-OpenGL", 63);
-    }
-    if (strCurLanguage == 3)
-    {
-        strncpy(dev->name, "OpenGL predeterminado", 63);
-    }
-    else if (strCurLanguage == 4)
-    {
-        strncpy(dev->name, "OpenGL di Default", 63);
-    }
-    else
-    {
-        strncpy(dev->name, "Default OpenGL", 63);
-    }
-    dev->modes = NULL;
-    //try good old DirectDraw <= 3 enumeration
-    memset(&primaryDev, 0, sizeof(rdevice));
-    primaryVal = rinEnumeratePrimary(&primaryDev);
-    if (primaryVal)
-    {
-        rinCopyModes(dev, &primaryDev);
-    }
-    else
+
+    // Enumerate video modes. If it fails, assume certain modes are supported
+    if ( ! rinEnumeratePrimary(dev))
     {
         //assume these are supported
-        rinAddMode(dev,  640, 480, 16);
-        rinAddMode(dev,  800, 600, 16);
-        rinAddMode(dev, 1024, 768, 16);
+        rinAddMode(dev,  640,  480, 24);
+        rinAddMode(dev,  800,  600, 24);
+        rinAddMode(dev, 1024,  768, 24);
+        rinAddMode(dev, 1280,  720, 24);
+        rinAddMode(dev, 1920, 1080, 24);
     }
+
     rinSortModes(dev);
-    gldev = dev;
-    nDevices++;
-
-    if (!(gDevcaps & DEVSTAT_NOGL_9X))
-    {
-        rinAddDevice(gldev);
-    }
-
-    //success
-    return 1;
+    rinAddDevice(dev);
+    nDevices = 1;
+    return 1; //success
 }
