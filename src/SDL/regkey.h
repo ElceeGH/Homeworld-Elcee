2617911a3b59b73b1fa2a735efc75fbe4460be4c
Elcee <elcee.github@gmail.com>
2021-09-17 20:55:24 +0100

    Config file location: - On windows, use "Homeworld" and not ".homeworld", nothing uses the dot on Windows. - Specify the string for getenv() instead of ifdefing in the code.

--- src/SDL/regkey.h
+++ src/SDL/regkey.h
@@ -7,33 +7,45 @@
 #ifndef ___REGKEY_H
 #define ___REGKEY_H
 
-// Do not change these registry key definitions! The installer depends on them...
+/*
+    This used to define the Windows registry keys and such.
+    The game no longer uses the registry, so now it defines the environment
+    variable to query for storing user data and the config directory names
+    for each platform.
+*/
+
 #if defined(HW_GAME_DEMO)
-
-    #define BASEKEYNAME "SOFTWARE\\Sierra On-Line\\Homeworld Downloadable"
-    #define CONFIGDIR ".homeworldDownloadable"
-
-#elif defined(HW_GAME_RAIDER_RETREAT)
-
-    #define BASEKEYNAME "SOFTWARE\\Sierra On-Line\\Homeworld OEM"
-	
     #ifdef _MACOSX
-        // Raider Retreat and the full version can live alongside so we use the same dir
-        #define CONFIGDIR "Library/Application Support/Homeworld"
+        #define CONFIGDIR "Library/Application Support/HomeworldDownloadable"
+    #elif defined(_WINDOWS)
+        #define CONFIGDIR "HomeworldDownloadable"
     #else
-        #define CONFIGDIR ".homeworldOEM"
+        #define CONFIGDIR ".homeworldDownloadable"
     #endif
 
 #else
 
-    #define BASEKEYNAME "SOFTWARE\\Sierra On-Line\\Homeworld"
-
     #ifdef _MACOSX
         #define CONFIGDIR "Library/Application Support/Homeworld"
+    #elif defined(_WINDOWS)
+        #define CONFIGDIR "Homeworld"
     #else
         #define CONFIGDIR ".homeworld"
     #endif
 
 #endif
 
+
+
+#ifdef _WINDOWS
+    #define APPDATADIR "APPDATA"
+#else
+    #define APPDATADIR "HOME"
 #endif
+
+
+
+#endif
+
+
+
