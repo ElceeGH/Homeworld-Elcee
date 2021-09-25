1fe91c7e10111bfbf3ac15d5e890a67fe530ed4f
Elcee <elcee.github@gmail.com>
2021-09-25 21:18:57 +0100

    HorseRace: Use linear filtering on the image. Images scaled with nearest neighbour sampling look absolutely godawful when the scale is not an integer.

--- src/Game/HorseRace.c
+++ src/Game/HorseRace.c
@@ -704,12 +704,11 @@ void hrInitBackground(void)
         glGenTextures(1, &hrBackgroundTexture);
         trClearCurrent();
         glBindTexture(GL_TEXTURE_2D, hrBackgroundTexture);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, hrBackXSize, hrBackYSize,
-                    0, GL_RGB, GL_UNSIGNED_BYTE, pTempImage);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, hrBackXSize, hrBackYSize, 0, GL_RGB, GL_UNSIGNED_BYTE, pTempImage);
         
         memFree(jp.ptr);
         memFree(pTempImage);
