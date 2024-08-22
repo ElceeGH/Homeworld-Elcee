62dc01a37d1f20c188d61efb7e0e2d4ce68ace73
Elcee <elcee.github@gmail.com>
2024-08-22 19:40:08 +0100

    Whoops, meant to fix these absolute paths 3 years ago

--- src/Game/libs.c
+++ src/Game/libs.c
@@ -1,13 +1,13 @@
 
 
 
-#pragma comment( lib, "C:/Projects/HomeworldSDL/SDL/VisualC/Win32/Debug/SDL2.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/SDL/VisualC/Win32/Debug/SDL2main.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/SDL_net/lib/x86/SDL2_net.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avcodec.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avformat.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/avutil.lib")
-#pragma comment( lib, "C:/Projects/HomeworldSDL/libav/lib/swscale.lib")
+#pragma comment( lib, "../../SDL/VisualC/Win32/Debug/SDL2.lib")
+#pragma comment( lib, "../../SDL/VisualC/Win32/Debug/SDL2main.lib")
+#pragma comment( lib, "../../SDL_net/lib/x86/SDL2_net.lib")
+#pragma comment( lib, "../../libav/lib/avcodec.lib")
+#pragma comment( lib, "../../libav/lib/avformat.lib")
+#pragma comment( lib, "../../libav/lib/avutil.lib")
+#pragma comment( lib, "../../libav/lib/swscale.lib")
 #pragma comment( lib, "opengl32.lib")
 
 
